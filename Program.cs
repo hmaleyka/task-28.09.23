b4e471b580da0a1dbaf6c13d30b5fe9fcb8da92d
Maleyka heybatova <maleykaheybatova1011@gmail.com>
2023-09-28 00:48:10 +0400

    the codes of task1 and task2 were added

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿//TASK1
+//int n = 2048;
+//while (n % 2 == 0 && n>0)
+//{
+//    n /= 2;
+//}
+//if (n == 1)
+//{
+//    Console.WriteLine("it is power of 2");
+//}
+//else
+//{
+//    Console.WriteLine("it is not  power of the 2");
+//}
+
+//TASK2
+
+//int n = 9, a = 0;  // a is the count the number of divisons
+
+
+//for (int i = 1; i <= n; i++)
+//{
+//    if (n % i == 0)  // n is divided by only 1 and has only 1 divison ,after  i was increased to find the second divison of the number
+//                     // when a = 2 , it means that the simple number has only 2 divisons
+//    {
+//        a++;
+//    }
+//}
+//if (a == 2)
+//{
+//    Console.WriteLine(" it is not complex Number", n);
+//}
+//else
+//{
+//    Console.WriteLine("it is complex number");
+//}
